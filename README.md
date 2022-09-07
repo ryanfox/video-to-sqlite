5748265aa0d44e84c57665cc3ea37fd3a20e1c87
Ryan Fox <ryan@foxrow.com>
2022-09-06 23:29:33 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# video-to-sqlite
+
+[![PyPI](https://img.shields.io/pypi/v/video-to-sqlite.svg)](https://pypi.org/project/video-to-sqlite/)
+
+Load data about a video file into SQLite
+
+## Installation
+
+Install this tool using `pip`:
+
+    pip install video-to-sqlite
+
+## Usage
+
+To create a SQLite database with metadata about a video, run:
+
+    video-to-sqlite videos.db my_video.mp4
+
+The tool will create two tables: `videos` and `frames`. If `videos.db` already exists, the video will be added to the
+`videos` table.
+
+To create the tables with a prefix, use `--prefix prefix`. For example:
+
+    video-to-sqlite videos.db cool_video.mp4 --prefix cool_
+
+This will create tables called `cool_videos` and `cool_frames`.
+
+### Extra processing
+
+Using the library programatically allows extra columns to be added to the database via a callback. Specify the
+`callback` argument to `cli.main()`.
+
+The function will be called once per frame in the video. It should accept a numpy ndarray as the single argument, and
+return a dict. The dict's keys will be added as columns to the DB, and values will be the corresponding values for that
+row.
+
+An example:
+
+```
+import video_to_sqlite
+
+def my_callback(frame):
+    has_a_cat_in_it = ai_cat_image_detector(frame)
+    return {'cat': has_a_cat_in_it}
+
+video_to_sqlite.main('my_database.db', 'cool_video.mp4', 'cool_', my_callback)
+```
+
+See `callback.py` for a working example.
+
+
+## video-to-sqlite --help
+
+```
+Usage: video-to-sqlite DB_PATH VIDEO_FILE PREFIX
+
+  Load data about Python packages from PyPI into SQLite
+
+  Usage example:
+
+      video-to-sqlite videos.db my_video.mp4 my_
+
+  Created tables will be videos and frames
+
+  To create tables called cool_videos, cool_frames use
+  --prefix cool_:
+
+      video-to-sqlite videos.db my_video.mp4 --prefix cool_
+
+Options:
+  --version            Show the version and exit.
+  --prefix TEXT        Prefix to use for the created database tables
+  --help               Show this message and exit.
+
+```
---
+++ video_to_sqlite/cli.py
@@ -0,0 +1,134 @@
+import click
+import imageio.v3 as iio
+import re
+import sqlite_utils
+import subprocess
+
+from sqlite_utils.utils import TypeTracker
+
+
+@click.command()
+@click.version_option()
+@click.argument(
+    'db_path',
+    type=click.Path(file_okay=True, dir_okay=False, allow_dash=False),
+    required=True,
+)
+@click.argument(
+    'video_file',
+    type=click.Path(file_okay=True, dir_okay=False),
+)
+@click.option(
+    '--prefix',
+    help='Prefix to use for the created database tables',
+    default='',
+)
+def cli(db_path, video_file, prefix):
+    """
+    Load data about frames from a video into SQLite
+
+    Usage example:
+
+        video-to-sqlite videos.db my_video.mp4
+
+    Created tables will be named `videos` and `frames`
+
+    To create tables called my_videos, my_frames use --prefix my_:
+
+        video-to-sqlite videos.db my_video.mp4 --prefix my_
+    """
+    main(db_path, video_file, prefix)
+
+
+def main(db_path, video_filename, prefix, callback=None):
+    """
+    Extract info from a video file and write it to an sqlite DB.
+    
+    By default, the `frames` table contains metadata about each frame.
+    To include additional columns, supply a function in the `callback`
+    agrument.
+    
+    `callback` is an optional argument for running additional
+    processing on each frame of the video.
+    If supplied, the function should accept a numpy ndarray of the frame as
+    the single argument, and return a dict.
+    Keys in the returned dict will become columns in the database, and values
+    will be that column's value for the frame.
+
+    See callback.py for an example.
+    """
+    db = sqlite_utils.Database(db_path)
+    metadata, frames = parse_video(video_filename, callback)
+    save_to_db(db, video_filename, metadata, frames, prefix)
+
+
+def parse_video(video_filename, callback=None):
+    process = subprocess.run(['ffprobe', '-hide_banner', video_filename], capture_output=True)
+    stderr = process.stderr.decode()
+
+    video_line = [line for line in stderr.splitlines() if line.strip().startswith('Stream #') and 'Video' in line][0].strip()
+    video_stats = video_line.split(',')
+    codec = video_stats[0].split('Video: ')[1]
+    pixel_format = video_stats[1].strip().split('(')[0]
+    resolution = video_stats[3].split()[0]
+    framerate = video_stats[5].split()[0]
+
+    metadata = {
+        'duration': re.search(r'Duration: (\d\d:\d\d:\d\d\.\d\d)', stderr).groups()[0],
+        'bitrate': re.search(r'bitrate: (\d+ [kM]?b/s)', stderr).groups()[0],
+        'codec': codec,
+        'pixel_format': pixel_format,
+        'resolution': resolution,
+        'framerate': framerate,
+    }
+
+    process = subprocess.run(['ffprobe', '-show_frames', video_filename], capture_output=True)
+    stdout = process.stdout.decode()
+    frames = stdout.split('[/FRAME]')
+
+    processed_frames = []
+    for raw_frame in frames:
+        lines = raw_frame.split('\n')[1:-1]  # drop the opening and closing "[FRAME]" tags
+        frame = {line.split('=')[0]: line.split('=')[1] for line in lines if '=' in line}
+
+        if frame.get('media_type', None) == 'video':
+
+            if frame['pkt_dts'] == 'N/A':
+                continue  # last frame doesn't get displayed or something?
+
+            frame['filename'] = video_filename
+            processed_frames.append(frame)
+
+    if callback:
+        for frame, frame_metadata in zip(iio.imiter(video_filename), processed_frames):
+            frame_metadata.update(callback(frame))
+
+    return metadata, processed_frames
+
+
+def save_to_db(db, video_filename, metadata, frames, prefix):
+    data = {
+        'filename': video_filename,
+        'duration': metadata['duration'],
+        'bitrate': metadata['bitrate'],
+        'codec': metadata['codec'],
+        'pixel_format': metadata['pixel_format'],
+        'resolution': metadata['resolution'],
+        'framerate': float(metadata['framerate']),
+    }
+
+    db[f'{prefix}videos'].insert(
+        data,
+        pk='filename',
+        replace=True,
+    )
+
+    frame_tracker = TypeTracker()
+    db[f'{prefix}frames'].insert_all(
+        frame_tracker.wrap([dict(frame, **{'frame_no': i}) for i, frame in enumerate(frames)]),
+        pk=('frame_no', 'filename'),
+        foreign_keys=[
+            ('filename', f'{prefix}videos', 'filename'),
+        ],
+        replace=True)
+    db[f'{prefix}frames'].transform(types=frame_tracker.types)
